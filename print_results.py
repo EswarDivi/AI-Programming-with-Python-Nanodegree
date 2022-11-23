1c07b2fa0207add9224656f18ab9122395f4f2e1
Eswar Divi <eswar.divi.902@gmail.com>
2022-11-23 09:29:00 +0530

    code_refactor

--- print_results.py
+++ print_results.py
@@ -93,17 +93,18 @@ def print_results(
                     )
                 )
 
-    if (print_incorrect_breed and 
-        (results_stats_dic['n_correct_dogs'] != results_stats_dic['n_correct_breed']) 
-       ):
+    if print_incorrect_breed and (
+        results_stats_dic["n_correct_dogs"] != results_stats_dic["n_correct_breed"]
+    ):
         print("\nINCORRECT Dog Breed Assignment:")
 
         # process through results dict, printing incorrectly classified breeds
         for key in results_dic:
 
             # Pet Image Label is-a-Dog, classified as-a-dog but is WRONG breed
-            if ( sum(results_dic[key][3:]) == 2 and
-                results_dic[key][2] == 0 ):
-                print("Real: {:>26}   Classifier: {:>30}".format(results_dic[key][0],
-                                                          results_dic[key][1]))
- 
+            if sum(results_dic[key][3:]) == 2 and results_dic[key][2] == 0:
+                print(
+                    "Real: {:>26}   Classifier: {:>30}".format(
+                        results_dic[key][0], results_dic[key][1]
+                    )
+                )
